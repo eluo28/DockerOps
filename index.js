fa904628136092c97bcbf86b3f88838881718afd
Edwin Luo <edwinluo28@gmail.com>
2021-04-30 18:18:33 -0400

    implemented mongo as another service

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const {
+  MONGO_USER,
+  MONGO_PASSWORD,
+  MONGO_IP,
+  MONGO_PORT,
+} = require("./config/config");
 
 const app = express();
 
+const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
+
+mongoose
+  .connect(mongoURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log("connected to DB"))
+  .catch((e) => console.log(e));
+
 app.get("/", (req, res) => {
-  res.send("<h1>helelo worrrld<h1>");
+  res.send("<h1>hello worrrld<h1>");
 });
 
 const port = process.env.PORT || 3000;
